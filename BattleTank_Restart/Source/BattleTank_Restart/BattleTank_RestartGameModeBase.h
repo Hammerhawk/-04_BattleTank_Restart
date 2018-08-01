bb1762061040ac2a0b0866e2015ad71410fd6ee0
DESKTOP-M5HVBEG\DELL <kyfries@yahoo.de>
2018-08-01 14:50:57 +0200

    TerrainModeled

---
+++ BattleTank_Restart/Source/BattleTank_Restart/BattleTank_RestartGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTank_RestartGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_RESTART_API ABattleTank_RestartGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
